e5d9856c670cd2d08ef18c6cee8bf58e5279a2d7
EUli16 <ethan.uliano@gmail.com>
2023-04-08 12:57:54 -0400

    created base modules

---
+++ backend_SDES_modules/KeyGeneration.py
@@ -0,0 +1,14 @@
+#key generation module
+
+class KeyGeneration:
+#the first method, the user must select a 10 bit key, i will
+#create an empty method as the front end will handle this user input
+#must ensure it is a sequence of 1's and 0's
+    def select_key(self):
+        pass
+
+    def randomize_bits(self, int: its):
+
+
+
+
